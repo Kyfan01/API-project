945cb625e3aac96a38abfd1fef32dd7c18262aee
Kevin Fan <kyfan01@gmail.com>
2023-12-22 16:43:14 -0500

    options objects tweaks

--- backend/db/migrations/20231221234614-create-group.js
+++ backend/db/migrations/20231221234614-create-group.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(options, {
+    await queryInterface.createTable('Groups', {
       id: {
         allowNull: false,
         autoIncrement: true,
