93fa9e00f5c389b73d34c28edec17eaa4d134b82
Kevin Fan <kyfan01@gmail.com>
2024-01-02 12:26:29 -0500

    local group endpoint works

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const { restoreUser } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const groupsRouter = require('./groups.js')
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
@@ -15,6 +16,8 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/groups', groupsRouter)
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
