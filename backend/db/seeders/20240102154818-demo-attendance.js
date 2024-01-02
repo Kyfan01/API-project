24035df6b4839932106e66b99c973ea4c6716951
Kevin Fan <kyfan01@gmail.com>
2024-01-02 10:54:30 -0500

    attendance

---
+++ backend/db/seeders/20240102154818-demo-attendance.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const { Group } = require('../models');
+
+let options = { tableName: 'Groups' };
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await Group.bulkCreate([
+      {
+        eventId: 1,
+        userId: 4,
+        status: 'enum here'
+      },
+      {
+        eventId: 2,
+        userId: 5,
+        status: 'enum here'
+      },
+      {
+        eventId: 3,
+        userId: 6,
+        status: 'enum here'
+      },
+      {
+        eventId: 4,
+        userId: 7,
+        status: 'enum here'
+      },
+      {
+        eventId: 5,
+        userId: 8,
+        status: 'enum here'
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      eventId: { [Op.in]: [1, 2, 3, 4, 5] }
+    }, {});
+  }
+};
